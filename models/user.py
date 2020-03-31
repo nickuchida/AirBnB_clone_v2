4e2083dc2e2f6d504a5f1598753ba228197ae801
nickuchida <nicholas.y.uchida@gmail.com>
2020-03-31 03:21:01 +0000

    add columns to user

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class User(BaseModel):
     """This is the class for user
@@ -11,7 +12,11 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", backref="user", cascade="all,delete")
+    reviews = relationship("Review", backref="user", cascade="all,delete")
