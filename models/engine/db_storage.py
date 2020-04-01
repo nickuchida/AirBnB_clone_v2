cf03e4c5dbcae6ec17ab53ab78275365230b2647
Ben Keener <keener4christ@gmail.com>
2020-04-01 00:24:51 +0000

    Added ammenity to the class list

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,7 +19,7 @@ class DBStorage:
     # TODO:
     # TODO: VERY IMPORTANT ADD ALL CLASSES BACK
     # TODO:
-    all_classes = (User, State, City, Place, Review)
+    all_classes = (User, State, City, Place, Review, Amenity)
 
     def __init__(self):
         '''initialize db engine'''
