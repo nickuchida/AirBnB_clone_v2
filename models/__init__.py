7958417451e5ef5a4aec8fc4366c4ea235de31c3
Ben Keener <keener4christ@gmail.com>
2020-04-01 00:17:51 +0000

    Removed debugging output

--- models/__init__.py
+++ models/__init__.py
@@ -7,10 +7,8 @@ storage = None
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    print("Using db")
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    print("Using fs")
 
 storage.reload()
