ae1fabe8bff9583bbf7a73bb907b46a8a33f9c91
nickuchida <nicholas.y.uchida@gmail.com>
2020-04-02 18:51:40 +0000

    import db storage

--- models/__init__.py
+++ models/__init__.py
@@ -9,6 +9,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 storage = None
 
