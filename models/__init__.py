c2dad7bfc06d8b407bcc2251b6a4831782d182fa
nickuchida <nicholas.y.uchida@gmail.com>
2020-04-02 18:48:58 +0000

    import basemodel

--- models/__init__.py
+++ models/__init__.py
@@ -7,6 +7,7 @@ from models.review import Review
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
+from models.base_model import BaseModel
 
 storage = None
 
