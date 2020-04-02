4eb6f035855182adaae7ecc177a885c2183f1eb3
Ben Keener <keener4christ@gmail.com>
2020-04-02 05:21:15 +0000

    Changed column order

--- models/city.py
+++ models/city.py
@@ -12,8 +12,8 @@ class City(BaseModel, Base):
         name: input name
     """
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
 
     places = relationship("Place", backref="cities",
                           cascade="all,delete-orphan")
