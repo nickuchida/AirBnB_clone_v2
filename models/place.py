0adf4e51a4dcad872b509f737c3b08eead69292e
nickuchida <nicholas.y.uchida@gmail.com>
2020-04-02 21:27:26 +0000

    now trying without name

--- models/place.py
+++ models/place.py
@@ -60,7 +60,7 @@ class Place(BaseModel, Base):
 
         @amenities.setter
         def amenities(self, obj):
-            if obj and type(obj).__name__ == "Amenity":
+            if obj and type(obj) == "Amenity":
                 self.amenity_ids.append(obj.id)
 
         @property
