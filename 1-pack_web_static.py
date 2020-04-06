91ddb21f197890ff54f24a377e9af67f9f46a229
Ben Keener <keener4christ@gmail.com>
2020-04-06 18:40:03 +0000

    Completed task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+import datetime
+import os
+from fabric.api import local
+
+
+def do_pack():
+    if not os.path.isdir("./versions"):
+        os.makedirs("./versions")
+    ntime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    local("tar -czzf versions/web_static_{}.tgz web_static/*".format(ntime))
+    return ("{}/versions/web_static_{}.tgz".format(os.path.dirname(
+        os.path.abspath(__file__)), ntime))
