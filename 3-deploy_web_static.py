9585ad5cdfe75a2bace4fba4f985c00954a7e9a6
Ben Keener <keener4christ@gmail.com>
2020-04-07 04:18:19 +0000

    Changed to full path

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -39,7 +39,8 @@ def do_pack():
         os.makedirs("./versions")
     ntime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     local("tar -czzf versions/web_static_{}.tgz web_static/*".format(ntime))
-    return ("versions/web_static_{}.tgz".format(os.path.dirname(ntime)))
+    return ("{}/versions/web_static_{}.tgz".format(os.path.dirname(
+        os.path.abspath(__file__)), ntime))
 
 
 def deploy():
